8a7c9837f70ad26266b8d76eb64ab3addae58017
Fehmi Anaç <fehmi_anac@hotmail.com>
2016-11-26 23:01:50 +0200

    sipariş listesi.

--- fashion-carnaval-poa/src/pages/order/last-order.ts
+++ fashion-carnaval-poa/src/pages/order/last-order.ts
@@ -12,7 +12,18 @@ import { CustomerListPage } from '../customer/customer-list'
 })
 export class LastOrderPage extends BasePage {
 
+    customerList = [];
     constructor(public navCtrl: NavController, public multiLanguage: MultiLanguage, public globalVariables: GlobalVariables) {
         super(multiLanguage, globalVariables);
+        this.customerList.push({ "Id": "2", "Name": "Deneme1", "City": "İstanbul" });
+        this.customerList.push({ "Id": "2", "Name": "Deneme2", "City": "İstanbul" });
+        this.customerList.push({ "Id": "2", "Name": "Deneme3", "City": "İstanbul" });
+        this.customerList.push({ "Id": "2", "Name": "Deneme4", "City": "İstanbul" });
+        this.customerList.push({ "Id": "2", "Name": "Deneme5", "City": "İstanbul" });
+        this.customerList.push({ "Id": "2", "Name": "Deneme6", "City": "İstanbul" });
+    }
+
+    searchLastOrder(event: any) {
+
     }
 }
